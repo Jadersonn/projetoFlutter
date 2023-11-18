d4a2a8046dfc2342292e6cbac28cc993c1b745bf
Sidowe102 <jaderson6100@gmail.com>
2023-11-17 19:40:08 -0500

    adicionando o projeto para o repositorio

---
+++ fluttercrud/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:fluttercrud/models/user.dart';
+
+Map<String, User> DUMMY_USERS = {
+  '1' : User(
+    id: '1',
+    nome: "Daniel",
+    email: "daniel@email.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2022/05/14/07/09/man-7195055_1280.png",
+  ),
+  '2' : User(
+    id: '2',
+    nome: "Joffrey",
+    email: "joffrey@kingsland.com",
+  ),
+  '3' : User(
+    id: '3',
+    nome: "Galdalf",
+    email: "galdalf@middleearth.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2022/05/14/07/09/man-7195055_1280.png",
+  ),
+  '4' : User(
+    id: '4',
+    nome: "Wesker",
+    email: "wesker@umbrella.com",
+    avatarUrl: "https://cdn.pixabay.com/photo/2022/03/18/09/06/face-7076175_1280.png",
+  ),
+  
+};
